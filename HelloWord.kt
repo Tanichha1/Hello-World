7501e5b9db3e08070e52f7c0f724f4e6c0c388cf
Tanichha1 <53221864+Tanichha1@users.noreply.github.com>
2020-05-10 18:13:40 +0530

    Delete HelloWord.kt

--- HelloWord.kt
+++
@@ -1,9 +0,0 @@
-//empty space
-// single line comment
-/*multi line comment*/
-fun main() {
-    println("Hello World") //ln--line print then shifts to new line
-    print("'Hello,World'")
-
-    //program code comes here
-}
