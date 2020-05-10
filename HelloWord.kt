8439d1cefbf7a7b1f6e24f66e45afc33686bba5e
Tanichha1 <53221864+Tanichha1@users.noreply.github.com>
2020-05-10 18:12:43 +0530

    Add files via upload

---
+++ HelloWord.kt
@@ -0,0 +1,9 @@
+//empty space
+// single line comment
+/*multi line comment*/
+fun main() {
+    println("Hello World") //ln--line print then shifts to new line
+    print("'Hello,World'")
+
+    //program code comes here
+}
